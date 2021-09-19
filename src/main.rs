930a1db10d7abbafd2a6516f701f8a809c3bbd81
k.nebogin <kinebogin@gmail.com>
2021-09-19 23:33:13 +0300

    Connect to gRPC server, call streaming method SubscribeOn and read events

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ mod settings;
 use settings::Settings;
 use mongodb::{Client, options::ClientOptions, options::Credential, options::ServerAddress, bson::doc};
 
-pub mod wavesenterprise {
+pub mod we {
     tonic::include_proto!("wavesenterprise");
 }
 
 #[allow(unused_imports)] //TODO: remove this later please
-use wavesenterprise::blockchain_events_service_client;
+use we::blockchain_events_service_client as bc_client;
 
 #[tokio::main]
-async fn main() -> mongodb::error::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Crusty starting...");
     let config_path = env::args()
         .nth(1)
@@ -43,5 +43,34 @@ async fn main() -> mongodb::error::Result<()> {
         .await?;
     println!("MongoDB Connected successfully.");
 
+    let node_endpoint = format!("http://{host}:{port}", host = setting.node.ip, port = setting.node.port);
+
+    let mut node_client = bc_client::BlockchainEventsServiceClient::connect(node_endpoint).await?;
+
+    let start_request = we::SubscribeOnRequest {
+        start_from: Some(we::subscribe_on_request::StartFrom::GenesisBlock(we::GenesisBlock {})),
+        // start_from: Some(we::subscribe_on_request::StartFrom::CurrentEvent(we::CurrentEvent {})),
+        events_filters: Vec::new()
+    };
+
+    match node_client.subscribe_on(start_request).await {
+        Err(status) => println!("Failed during SubscribeOn: {}", status),
+        Ok(response) => {
+            let mut events = response.into_inner();
+            let mut counter: i8 = 1;
+
+            // Let's just see 5 events for now
+            while let Some(event) = events.message().await? {
+                println!("[counter {}] Received {:?}", counter, event);
+                counter += 1;
+                if counter > 5 {
+                    break;
+                }
+            }
+            println!("Outside of while let loop");
+        }
+    }
+
+    println!("So long!");
     Ok(())
 }
