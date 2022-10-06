1c7041c2e0f5f72519648a806426e8939d8ab82b
kinebogin <kinebogin@gmail.com>
2022-10-06 23:31:52 +0400

    Updating libraries and Rust edition, part 2

--- src/settings.rs
+++ src/settings.rs
@@ -4,13 +4,13 @@ use config::{ConfigError, Config, File, Environment};
 #[derive(Deserialize, Debug)]
 pub struct Settings {
     pub node: NodeCfg,
-    pub mongo: MongoCfg
+    pub mongo: MongoCfg,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct NodeCfg {
     pub ip: String,
-    pub port: u16
+    pub port: u16,
 }
 
 #[derive(Deserialize, Debug)]
@@ -18,25 +18,21 @@ pub struct MongoCfg {
     pub ip: String,
     pub port: Option<u16>,
     pub user: String,
-    pub password: String
+    pub password: String,
 }
 
 impl Settings {
     pub fn new(config_path: &str) -> Result<Self, ConfigError> {
-        let mut s = Config::default();
-
-        // Read application's default config
-        s.merge(File::with_name(DEFAULT_CONFIG_PATH))?;
-
-        // Read user provided config file
-        s.merge(File::with_name(config_path))?;
-
-        // Read settings from the ENV with a "CRUSTY" prefix and an underscore separator
-        // E.g.: `CRUSTY_MONGO_PASSWORD=qwerty` should override `mongo.password` setting
-        s.merge(Environment::with_prefix("CRUSTY").separator("_"))?;
-
-        // Finally, deserialization into the Settings struct
-        s.try_into()
+        Config::builder()
+            // Read application's default config
+            .add_source(File::with_name(DEFAULT_CONFIG_PATH))
+            // Read user provided config file
+            .add_source(File::with_name(config_path))
+            // Read settings from the ENV with a "CRUSTY" prefix and an underscore separator
+            // E.g.: `CRUSTY_MONGO_PASSWORD=qwerty` should override `mongo.password` setting
+            .add_source(Environment::with_prefix("CRUSTY").separator("_"))
+            .build()?
+            .try_deserialize()
     }
 }
 
